c1ce6f7991e9ff130d7f66ffd79b2d9486b03840
Aditya jha <medoremon0001@gmail.com>
2025-02-13 22:35:12 +0530

    Create get_post.js

---
+++ frontend-app/src/get_post.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function GetPost(){
+    return(
+        <div>
+            <h2>Get Post</h2>
+            <p>Click the button below to get a post</p>
+            <button>Get Post</button>
+        </div>
+    );
+}
+export default GetPost;
