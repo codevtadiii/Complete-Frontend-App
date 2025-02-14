a6e63a15944ed59969d5a3c4c4f07b85074ba2eb
Aditya jha <medoremon0001@gmail.com>
2025-02-14 14:51:59 +0530

    Update get_post.js

--- frontend-app/src/get_post.js
+++ frontend-app/src/get_post.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import axios from 'axios';
+import './navbar.css';
+
+
 
 function GetPost(){
+    const[data, setdata]= useState([]);
+    useEffect(()=>{
+        axios.get("https://jsonplaceholder.typicode.com/posts").then(response=>setdata(response.data)).catch(err=>console.log(err));
+
+    })
     return(
-        <div>
-            <h2>Get Post</h2>
-            <p>Click the button below to get a post</p>
-            <button>Get Post</button>
+        <div className="post">
+            {
+                data.map((obj)=>{
+                    return(
+                        <div className="design">
+                            <h2>{obj.title}</h2>
+                            <p>{obj.body}</p>
+                        </div>
+                    )
+                })
+            }
+            
         </div>
     );
 }
