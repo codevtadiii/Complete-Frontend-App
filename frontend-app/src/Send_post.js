1f80cc832de1e9d517865ba617ba8774d5e1b96e
Aditya jha <medoremon0001@gmail.com>
2025-02-17 09:37:24 +0530

    Update Send_post.js

--- frontend-app/src/Send_post.js
+++ frontend-app/src/Send_post.js
@@ -1,22 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import './sendpost.css';
+import "./sendpost.css";
 
 function SendPost() {
-    
+    const handleChange = (e)=>{
+        setFormData({
+            ...FormData,
+            [e.target.name]: e.target.value,
+        });
+        
+    }
+    const [FormData, setFormData] = useState({
+        title: "",
+        body: "",
+    });
+    function handleSubmit (e){
+        e.preventDefault();
+        axios.post('https://jsonplaceholder.typicode.com/posts', FormData)
+        .then((response)=>{
+            console.log(response);
+        })
+        
+    }
+
     return (
         <div className="send">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Title :</label>
-                <input type="text" name="title" placeholder="Enter title" required></input>
-                <br></br>
+                <input
+                    type="text"
+                    name="title"
+                    placeholder="Enter title"
+                    value={FormData.title}
+                    onChange={handleChange}
+                    required
+                />
+                <br />
                 <label>Body:</label>
-                <textarea type="text" name="body" placeholder="Enter body" required></textarea>
-                <br></br>
-                <button type="submit" >Send</button>
+                <textarea
+                    name="body"
+                    placeholder="Enter body"
+                    value={FormData.body}
+                    onChange={handleChange}
+                    required
+                />
+                <br />
+                <button type="submit">Send</button>
             </form>
-
         </div>
     );
 }
+
 export default SendPost;
